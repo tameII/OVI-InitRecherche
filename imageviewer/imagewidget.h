f662fc4772bda8dc6c10295bfff502858b2fcd56
Brandon.Hornbeck <brandon.hornbeck054@gmail.com>
2020-03-18 15:30:32 +0100

    Ajout du slider

---
+++ imageviewer/imagewidget.h
@@ -0,0 +1,16 @@
+#ifndef IMAGEWIDGET_H
+#define IMAGEWIDGET_H
+
+#include <QWidget>
+
+class ImageWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ImageWidget(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // IMAGEWIDGET_H
