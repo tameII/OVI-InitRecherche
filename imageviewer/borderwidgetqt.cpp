b9c12c1a99ea81e288ba0749c00cd8afdf1558c0
Alexis <alexis.cesaro.ac@gmail.Com>
2020-03-23 15:25:40 +0100

    Modification of .gitignore

---
+++ imageviewer/borderwidgetqt.cpp
@@ -0,0 +1,6 @@
+#include "borderwidgetqt.h"
+
+BorderWidgetQt::BorderWidgetQt()
+{
+
+}
---
+++ imageviewer/borderwidgetqt.h
@@ -0,0 +1,11 @@
+#ifndef BORDERWIDGETQT_H
+#define BORDERWIDGETQT_H
+
+
+class BorderWidgetQt
+{
+public:
+    BorderWidgetQt();
+};
+
+#endif // BORDERWIDGETQT_H
